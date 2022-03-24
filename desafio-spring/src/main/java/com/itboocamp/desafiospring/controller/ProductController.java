b78f8303403bf162dc17c77308f55db4fb07d709
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-03-24 17:38:25 -0300

    Implemented the Product Service to filter the products by category, and invoked it inside the ProductController

--- desafio-spring/src/main/java/com/itboocamp/desafiospring/controller/ProductController.java
+++ desafio-spring/src/main/java/com/itboocamp/desafiospring/controller/ProductController.java
@@ -1,9 +1,18 @@
 package com.itboocamp.desafiospring.controller;
 
+import com.itboocamp.desafiospring.repository.ProductRepository;
+import com.itboocamp.desafiospring.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+
+    public ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
 
 }
--- desafio-spring/src/main/java/com/itboocamp/desafiospring/service/ProductService.java
+++ desafio-spring/src/main/java/com/itboocamp/desafiospring/service/ProductService.java
@@ -1,4 +1,35 @@
 package com.itboocamp.desafiospring.service;
 
+import com.itboocamp.desafiospring.entity.Product;
+import com.itboocamp.desafiospring.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+@Service
 public class ProductService {
+
+    private ProductRepository  productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public ProductService() {
+
+    }
+
+
+    public List<Product> getProductsByCategory(String category){
+        List<Product> productList = productRepository.findAll();
+        return productList.stream()
+                .filter(product -> product.getCategory().toLowerCase().equals(category.toLowerCase()))
+                .collect(Collectors.toList());
+
+
+    }
+
 }
