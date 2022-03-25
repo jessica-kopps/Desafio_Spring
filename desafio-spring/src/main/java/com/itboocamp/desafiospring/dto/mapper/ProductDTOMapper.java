f8c9674b78cb1d76ec233e4d0b6420eb6cd4b392
mateusvictor <mateus.ssilva@mercadolivre.com>
2022-03-25 14:18:43 -0300

    add required imports

--- desafio-spring/src/main/java/com/itboocamp/desafiospring/dto/mapper/ProductDTOMapper.java
+++ desafio-spring/src/main/java/com/itboocamp/desafiospring/dto/mapper/ProductDTOMapper.java
@@ -1,9 +1,12 @@
 package com.itboocamp.desafiospring.dto.mapper;
 
 import com.itboocamp.desafiospring.dto.response.ProductResponseDTO;
+import com.itboocamp.desafiospring.dto.resquest.ProductPurchaseRequestDTO;
 import com.itboocamp.desafiospring.entity.Product;
+import com.itboocamp.desafiospring.repository.ProductRepository;
 
 public class ProductDTOMapper {
+    private final ProductRepository productRepository = new ProductRepository();
 
     public ProductResponseDTO mapDTO(Product entity) {
         return new ProductResponseDTO().builder()
@@ -17,9 +20,12 @@ public class ProductDTOMapper {
                 .prestige(entity.getPrestige()).build();
     }
 
-    public Product mapEntity(ProductResponseDTO productResponseDTO) {
-        return null;
+    public Product mapEntity(ProductPurchaseRequestDTO productPurchaseDTO) {
+        Product product = productRepository.findById(productPurchaseDTO.getProductId());
+        return product;
     }
+
+
 
 
 }
